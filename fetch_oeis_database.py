e257a97a844b6af3fb4f1945a84b8b5c67a768c6
Sidney Cadot <sidney@jigsaw.nl>
2015-09-07 14:20:40 +0200

    Changed DB to store first/last timestamps associated to the content. Updated "fetch" routine to retrieve based on score.

--- fetch_oeis_database.py
+++ fetch_oeis_database.py
@@ -11,35 +11,35 @@ from bs4 import BeautifulSoup, NavigableString
 
 logger = logging.getLogger(__name__)
 
-def oeis_internal_html_to_contents(html):
+def oeis_internal_html_to_content(html):
     """ Parse the OEIS html
     """
     soup = BeautifulSoup(html, 'html.parser')
-    contents = []
+    content = []
 
     for tt in soup.find_all("tt"):
 
-        contents_line = tt.contents
+        content_line = tt.contents
 
-        if len(contents_line) == 0:
+        if len(content_line) == 0:
             # Empty 'tt' tags do occur; we discard them.
             continue
 
-        assert len(contents_line) == 1
-        contents_line = contents_line[0]
+        assert len(content_line) == 1
+        content_line = content_line[0]
 
-        assert isinstance(contents_line, NavigableString)
-        contents_line = str(contents_line)
+        assert isinstance(content_line, NavigableString)
+        content_line = str(content_line)
 
-        assert contents_line.endswith("\n")
-        contents_line = contents_line[:-1]
+        assert content_line.endswith("\n")
+        content_line = content_line[:-1]
 
-        if contents_line.startswith("%"):
-            contents.append(contents_line)
+        if content_line.startswith("%"):
+            content.append(content_line)
 
-    contents = "\n".join(contents)
+    content = "\n".join(content)
 
-    return contents
+    return content
 
 class FetchFailure:
     ok = False
@@ -50,10 +50,10 @@ class FetchFailure404(FetchFailure):
 
 class FetchSuccess:
     ok = True
-    def __init__(self, oeis_id, timestamp, contents):
-        self.oeis_id = oeis_id
+    def __init__(self, oeis_id, timestamp, content):
+        self.oeis_id   = oeis_id
         self.timestamp = timestamp
-        self.contents = contents
+        self.content   = content
 
 def fetch_oeis_internal_entry(oeis_id):
 
@@ -81,9 +81,9 @@ def fetch_oeis_internal_entry(oeis_id):
         logger.error("Error while fetching '{}': {}".format(url, exception))
         return FetchFailure()
 
-    contents = oeis_internal_html_to_contents(html)
+    content = oeis_internal_html_to_content(html)
 
-    return FetchSuccess(oeis_id, timestamp, contents)
+    return FetchSuccess(oeis_id, timestamp, content)
 
 def find_highest_oeis_id():
 
@@ -115,10 +115,12 @@ def setup_schema(dbconn):
 
     schema = """
              CREATE TABLE IF NOT EXISTS oeis_entries (
-                 oeis_id    INTEGER  PRIMARY KEY NOT NULL,
-                 timestamp  REAL                 NOT NULL,
-                 contents   TEXT                 NOT NULL
-             )
+                 oeis_id       INTEGER  PRIMARY KEY NOT NULL, -- OEIS ID number.
+                 t_first_fetch REAL                 NOT NULL, -- earliest timestamp when the content below was first fetched.
+                 t_most_recent REAL                 NOT NULL, -- most recent timestamp when the content below was fetched.
+                 content       TEXT                 NOT NULL  -- content (i.e., lines starting with '%' sign).
+             );
+             CREATE INDEX IF NOT EXISTS oeis_entries_index ON oeis_entries(oeis_id);
              """
 
     schema = "\n".join(line[13:] for line in schema.split("\n"))[1:-1]
@@ -131,53 +133,97 @@ def fetch_entries_into_database(dbconn, entries):
     SLEEP_AFTER_BATCH = 5.0 # [seconds]
 
     pool = multiprocessing.Pool(NUM_PROCESSES)
+    try:
 
-    tStart = time.time()
-    nStart = len(entries)
+        tStart = time.time()
+        nStart = len(entries)
 
-    while len(entries) > 0:
+        while len(entries) > 0:
 
-        random_entries_count = min(FETCH_BATCH_SIZE, len(entries))
-        random_entries_to_be_fetched = random.sample(entries, random_entries_count)
+            random_entries_count = min(FETCH_BATCH_SIZE, len(entries))
+            random_entries_to_be_fetched = random.sample(entries, random_entries_count)
 
-        logger.info("Executing parallel fetch for {} out of {} entries.".format(len(random_entries_to_be_fetched), len(entries)))
+            logger.info("Executing parallel fetch for {} out of {} entries.".format(len(random_entries_to_be_fetched), len(entries)))
 
-        t1 = time.time()
-        results = pool.map(fetch_oeis_internal_entry, random_entries_to_be_fetched)
-        t2 = time.time()
+            t1 = time.time()
+            results = pool.map(fetch_oeis_internal_entry, random_entries_to_be_fetched)
+            t2 = time.time()
 
-        duration = (t2 - t1)
-        logger.info("{} parallel fetches took {:.3f} seconds ({:.3f} fetches/second).".format(len(random_entries_to_be_fetched), duration, len(random_entries_to_be_fetched) / duration))
+            duration = (t2 - t1)
+            logger.info("{} parallel fetches took {:.3f} seconds ({:.3f} fetches/second).".format(len(random_entries_to_be_fetched), duration, len(random_entries_to_be_fetched) / duration))
 
-        results = [result for result in results if result.ok]
+            # Process the fetch results.
 
-        logger.info("Inserting {} valid entries ({} bytes) in database ...".format(len(results), sum(len(result.contents) for result in results)))
+            countFailures         = 0
+            countNewEntries       = 0
+            countIdenticalEntries = 0
+            countUpdatedEntries   = 0
 
-        insert_entries = [(result.oeis_id, result.timestamp, result.contents) for result in results]
+            for result in results:
 
-        dbconn.executemany("INSERT OR REPLACE INTO oeis_entries(oeis_id, timestamp, contents) VALUES (?, ?, ?)", insert_entries)
-        dbconn.commit()
+                if not result.ok:
+                    # Skip entries that are not okay
+                    countFailures += 1
+                    continue
 
-        # Mark succesful entries as fetched
+                dbcursor = dbconn.cursor()
+                try:
+                    query = "SELECT content FROM oeis_entries WHERE oeis_id = ?;"
+                    dbcursor.execute(query, (result.oeis_id, ))
 
-        entries -= set(result.oeis_id for result in results)
+                    previous_content = dbcursor.fetchall()
 
-        tCurrent = time.time()
-        nCurrent = len(entries)
+                    assert len(previous_content) <= 1
+                    previous_content = None if len(previous_content) == 0 else previous_content[0][0]
 
-        ETC = (tCurrent - tStart) * nCurrent / (nStart - nCurrent)
-        logger.info("Estimated time to completion: {:.1f} minutes.".format(ETC / 60.0))
+                    if previous_content is None:
+                        # The oeis_id does not occur in the database, yet. We will insert it.
+                        query = "INSERT INTO oeis_entries(oeis_id, t_first_fetch, t_most_recent, content) VALUES (?, ?, ?, ?);"
+                        dbcursor.execute(query, (result.oeis_id, result.timestamp, result.timestamp, result.content))
+                        countNewEntries += 1
+                    elif previous_content != result.content:
+                        # The database content is stale.
+                        # Update t_first_fetch, t_most_recent, and content.
+                        query = "UPDATE oeis_entries SET t_first_fetch = ?, t_most_recent = ?, content = ? WHERE oeis_id = ?;"
+                        dbcursor.execute(query, (result.timestamp, result.timestamp, result.content, result.oeis_id))
+                        countUpdatedEntries += 1
+                    else:
+                        # The database content is identical to the freshly fetched content.
+                        # We will just update the t_most_recent field, indicating the fresh fetch.
+                        query = "UPDATE oeis_entries SET t_most_recent = ? WHERE oeis_id = ?;"
+                        dbcursor.execute(query, (result.timestamp, result.oeis_id))
+                        countIdenticalEntries += 1
+                finally:
+                    dbcursor.close()
 
-        # Sleep
+                # Mark entry as processed by removing it from the 'entries' set.
+                entries.remove(result.oeis_id)
 
-        logger.info("Sleeping for {:.1f} seconds ...".format(SLEEP_AFTER_BATCH))
-        time.sleep(SLEEP_AFTER_BATCH)
+            dbconn.commit()
+
+            logger.info("Processed {} results (failures: {}, new: {}, identical: {}, updated: {}).".format(len(results), countFailures, countNewEntries, countIdenticalEntries, countUpdatedEntries))
+
+            # Calculate and show estimated-time-to-completion.
+
+            tCurrent = time.time()
+            nCurrent = len(entries)
+
+            ETC = (tCurrent - tStart) * nCurrent / (nStart - nCurrent)
+            logger.info("Estimated time to completion: {:.1f} minutes.".format(ETC / 60.0))
+
+            # Sleep
+
+            logger.info("Sleeping for {:.1f} seconds ...".format(SLEEP_AFTER_BATCH))
+            time.sleep(SLEEP_AFTER_BATCH)
+    finally:
+        pool.close()
+        pool.join()
 
 def make_database_complete(dbconn, highest_oeis_id):
 
     dbcursor = dbconn.cursor()
     try:
-        dbcursor.execute("SELECT oeis_id FROM oeis_entries")
+        dbcursor.execute("SELECT oeis_id FROM oeis_entries;")
         entries_in_local_database = dbcursor.fetchall()
     finally:
         dbcursor.close()
@@ -192,26 +238,29 @@ def make_database_complete(dbconn, highest_oeis_id):
 
     fetch_entries_into_database(dbconn, missing_entries)
 
-def refresh_oldest_database_entries(dbconn, howmany):
+def update_database_entries_by_score(dbconn, howmany):
+
+    t_current = time.time()
 
     dbcursor = dbconn.cursor()
     try:
-        dbcursor.execute("SELECT oeis_id FROM oeis_entries ORDER BY timestamp LIMIT {}".format(howmany))
-        oldest_entries = dbcursor.fetchall()
+        query = "SELECT oeis_id FROM oeis_entries ORDER BY (? - t_most_recent) / max(t_most_recent - t_first_fetch, 1e-6) DESC LIMIT ?;"
+        dbcursor.execute(query, (t_current, howmany))
+        highest_score_entries = dbcursor.fetchall()
     finally:
         dbcursor.close()
 
-    oldest_entries = set(oeis_id for (oeis_id, ) in oldest_entries)
-    logger.info("Oldest entries in local database selected for refresh: {}".format(len(oldest_entries)))
+    highest_score_entries = set(oeis_id for (oeis_id, ) in highest_score_entries)
+    logger.info("Highes-score entries in local database selected for refresh: {}".format(len(highest_score_entries)))
 
-    fetch_entries_into_database(dbconn, oldest_entries)
+    fetch_entries_into_database(dbconn, highest_score_entries)
 
 def vacuum_database(dbconn):
 
     logger.info("Initiating VACUUM on database ...")
 
     t1 = time.time()
-    dbconn.execute("VACUUM")
+    dbconn.execute("VACUUM;")
     t2 = time.time()
 
     duration = (t2 - t1)
@@ -232,7 +281,7 @@ def main():
             setup_schema(dbconn)
             highest_oeis_id = find_highest_oeis_id()
             make_database_complete(dbconn, highest_oeis_id)
-            refresh_oldest_database_entries(dbconn, highest_oeis_id // 100) # refresh 1% of entries
+            update_database_entries_by_score(dbconn, highest_oeis_id // 200) # refresh 0.5% of entries
             vacuum_database(dbconn)
         finally:
             dbconn.close()
