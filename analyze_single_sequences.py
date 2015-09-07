01694d87d973cb37bce1d613f59b6d8fe1ded9c7
Sidney Cadot <sidney@jigsaw.nl>
2015-09-07 20:51:12 +0200

    The analyze script can now handle polynomial evaluations with an integer divisor.

--- analyze_single_sequences.py
+++ analyze_single_sequences.py
@@ -6,7 +6,20 @@ import warnings
 
 warnings.simplefilter('ignore')
 
-EPSILON = 1e-6
+EPSILON = 1e-10
+
+def gcd(a, b):
+    a = abs(a)
+    b = abs(b)
+    while a > 0:
+        (a, b) = (b % a, a)
+    return b
+
+def gcd_many(seq):
+    g = 0
+    for s in seq:
+        g = gcd(g, s)
+    return g
 
 with open("oeis.pickle", "rb") as f:
     entries = pickle.load(f)
@@ -23,7 +36,7 @@ for entry in entries:
     n = len(entry.values)
 
     if len(entry.values) < 5:
-        # unable to interpret offset
+        # don't try short sequences
         continue
 
     x = entry.offset[0] + np.arange(n)
@@ -46,7 +59,18 @@ for entry in entries:
 
     if len(fitpoly) <= n - 5:
 
-        yfit = np.polyval(fitpoly, x)
+        DENOM = 3628800 # == factorial(10)
+        fitpoly = np.round(fitpoly * 3628800).astype(np.int)
+
+        G = gcd_many(fitpoly)
+
+        if G == 0:
+            DENOM = 1
+        else:
+            fitpoly //= G
+            DENOM   //= G
+
+        yfit = np.polyval(fitpoly, x) / DENOM
 
         if np.all(np.abs(yfit - y) < EPSILON):
-            print("[{}] found polynomial relation: {} name: {}".format(entry, fitpoly, entry.name))
+            print("[{}] <len: {} first_index: {}> found polynomial relation: {} / {} -- name: {}".format(entry, len(entry.values), entry.first_index, fitpoly, DENOM, entry.name))
