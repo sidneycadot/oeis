3e5e4d2084bff4251c3eb754b658ad927501b36e
Sidney Cadot <sidney@jigsaw.nl>
2015-09-15 21:45:39 +0200

    Vetted sequences are now checked.

--- examples/vetted_sequences.py
+++ examples/vetted_sequences.py
@@ -1,6 +1,8 @@
-#! /usr/bin/env python3
+#/usr/bin/env python3
 
+from __future__ import print_function
 from collections import OrderedDict
+import pickle
 
 # =====================================================================
 
@@ -261,49 +263,103 @@ class TreeCountSequence(Sequence):
     def _value(self, index):
         return self._count_trees(1 + (self._fanout - 1) * (index - 1), 1)
 
-oeis_sequences = OrderedDict([
-          (     5 , lambda : CountDivisorsSequence()),
-          (    10 , lambda : EulerPhiSequence()),
-          (    32 , lambda : FibonacciLikeSequence([2, 1], [1, 1])),
-          (    40 , lambda : PrimeSequence()),
-          (    45 , lambda : FibonacciLikeSequence([0, 1], [1, 1])),
-          (    73 , lambda : FibonacciLikeSequence([0, 1, 1], [1, 1, 1])),
-          (   203 , lambda : SumDivisorsSequence()),
-          (   720 , lambda : PrimePiSequence()),
-          (  1045 , lambda : FibonacciLikeSequence([0, 1], [2, 1])),
-          (  1359 , lambda : TwinPrimeSequence()),
-          (  1590 , lambda : FibonacciLikeSequence([0, 1, 2], [1, 1, 1])),
-          (  2572 , lambda : TreeCountSequence(2)),
-          ( 10892 , lambda : FibonacciLikeSequence([1, 1], [-1, 1])),
-          ( 20639 , lambda : SmallestDivisorSequence()),
-          ( 48654 , lambda : FibonacciLikeSequence([1, 4], [1, 2])),
-          ( 85959 , lambda : PolynomialSequence(0, [0, 37])),
-          (104449 , lambda : FibonacciLikeSequence([3, 1], [1, 1])),
-          (176485 , lambda : TreeCountSequence(3)),
-          (176503 , lambda : TreeCountSequence(4)),
-          (194628 , lambda : TreeCountSequence(5)),
-          (194629 , lambda : TreeCountSequence(6)),
-          (194630 , lambda : TreeCountSequence(7)),
-          (194631 , lambda : TreeCountSequence(8)),
-          (194632 , lambda : TreeCountSequence(9)),
-          (194633 , lambda : TreeCountSequence(10))
-    ])
+oeis_sequences_poly = {
+         85959 : lambda : PolynomialSequence(0, [0, 37])
+    }
+
+oeis_sequences_fiblike = {
+            32 : lambda : FibonacciLikeSequence([2, 1], [ 1, 1]),
+            45 : lambda : FibonacciLikeSequence([0, 1], [ 1, 1]),
+          1045 : lambda : FibonacciLikeSequence([0, 1], [ 2, 1]),
+         10892 : lambda : FibonacciLikeSequence([1, 1], [-1, 1]),
+         48654 : lambda : FibonacciLikeSequence([1, 4], [ 1, 2]),
+        104449 : lambda : FibonacciLikeSequence([3, 1], [ 1, 1])
+    }
+
+oeis_sequences_numtheory = {
+             5 : lambda : CountDivisorsSequence(),
+            10 : lambda : EulerPhiSequence(),
+            40 : lambda : PrimeSequence(),
+           203 : lambda : SumDivisorsSequence(),
+           720 : lambda : PrimePiSequence(),
+          1359 : lambda : TwinPrimeSequence(),
+         20639 : lambda : SmallestDivisorSequence()
+    }
+
+oeis_sequences_tcs = {
+          2572 : lambda : TreeCountSequence(2),
+        176485 : lambda : TreeCountSequence(3),
+        176503 : lambda : TreeCountSequence(4),
+        194628 : lambda : TreeCountSequence(5),
+        194629 : lambda : TreeCountSequence(6),
+        194630 : lambda : TreeCountSequence(7),
+        194631 : lambda : TreeCountSequence(8),
+        194632 : lambda : TreeCountSequence(9),
+        194633 : lambda : TreeCountSequence(10)
+    }
+
+def _merge_dicts(*dicts):
+    merged = {}
+    for d in dicts:
+        for (k, v) in d.items():
+            assert k not in merged
+            merged[k] = v
+        del d
+    merged = OrderedDict(sorted(merged.items()))
+    return merged
+
+oeis_sequences = _merge_dicts(
+        oeis_sequences_poly
+        oeis_sequences_fiblike,
+        oeis_sequences_tcs
+        oeis_sequences_numtheory
+    )
 
 def main():
+
+    # Show sequence contents
+
     MAX_CHARS  = 200
-    for key in sorted(oeis_sequences.keys()):
+
+    for key in oeis_sequences.keys():
         sequence_maker = oeis_sequences[key]
         sequence = sequence_maker()
-
         print("A{:06d} : {}".format(key, sequence.sequence_string(max_chars = MAX_CHARS)))
 
     print()
 
-    for key in sorted(oeis_sequences.keys()):
+    # Show sequences
+
+    for key in oeis_sequences.keys():
         sequence_maker = oeis_sequences[key]
         sequence = sequence_maker()
-
         print("A{:06d} : {}".format(key, repr(sequence)))
+
+    print()
+
+    # Verify the sequences in our catalog.
+
+    print("Reading data ...")
+
+    filename = "../oeis_with_bfile.pickle"
+    #filename = "../oeis_with_bfile-10000.pickle"
+
+    with open(filename, "rb") as f:
+        oeis_entries = pickle.load(f)
+
+    print("Data read.")
+
+    for (oeis_id, sequence_maker) in oeis_sequences.items():
+        sequence = sequence_maker()
+        if (oeis_id - 1) >= len(oeis_entries):
+            print("[A{:06d}] OEIS entry not found in database, skipping.".format(oeis_id))
+            continue
+
+        oeis_entry = oeis_entries[oeis_id - 1]
+        assert sequence.first_index == oeis_entry.offset[0]
+        n = min(100, len(oeis_entry.values))
+        print("[A{:06d}] Checking {} of {} values ...".format(oeis_id, n, len(oeis_entry.values)))
+        assert all(sequence[sequence.first_index + i] == oeis_entry.values[i] for i in range(n))
 
 if __name__ == "__main__":
     main()
