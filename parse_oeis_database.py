a08defef0d608420fc137d416d44d78604a8fa7a
Sidney Cadot <sidney@jigsaw.nl>
2015-09-24 21:50:46 +0200

    Added format check.

--- parse_oeis_database.py
+++ parse_oeis_database.py
@@ -17,104 +17,59 @@ from timer     import start_timer
 
 logger = logging.getLogger(__name__)
 
-# As described here: https://oeis.org/eishelp1.html
-
-# The directives %S, %T, and %U were originally intended as the absolute values of the sequence entries,
-# with the signed entries given as %V, %W, %X. However, in the versions we downloaded, %S, %T, and %U
-# are signed, and %V, %W, and %X are never present.
-
-#expected_directives = [
-#    "%I", # Identification Line                                     (REQUIRED)
-#    "%S", # Beginning f the sequence (line 1 of 3)                  (REQUIRED)
-#    "%T", # Beginning f the sequence (line 2 of 3)
-#    "%U", # Beginning f the sequence (line 3 of 3)
-#    "%N", # Name of sequence                                        (REQUIRED)
-#    "%D", # Detailed references
-#    "%H", # Links related to this sequence
-#    "%F", # Formula
-#    "%Y", # Cross-references to other sequences
-#    "%A", # Author, submitter, or other Authority                   (REQUIRED)
-#    "%O", # Offset a, b                                             (REQUIRED)
-#    "%p", # Computer program to produce the sequence (Maple)
-#    "%t", # Computer program to produce the sequence (Mathematica)
-#    "%o", # Computer program to produce the sequence (other computer language)
-#    "%E", # Extensions and errors
-#    "%e", # Examples
-#    "%K", # Keywords                                                (REQUIRED)
-#    "%C"  # Comments
-#]
-
-# The order of expected directives, for any given entry is as follows:
-#
-# - First, a single "%I" entry.
-# - Next, either a single "%S" line, an "%S" line followed by a "%T" line, or an "%S" line followed by a "%T" line followed by a "%U" line.
-# - Next, either a single "%V" line, a "%V" line followed by a "%W" line, or a "%V" line followed by a "%W" line followed by an "%X" line.
-# - Next, a single "%N" line.
-# - Next, zero or more "%C" lines.
-# - Next, zero or more "%D" lines.
-# - Next, zero or more "%H" lines.
-# - Next, zero or more "%F" lines.
-# - Next, zero or more "%e" lines.
-# - Next, zero or more "%p" lines.
-# - Next, zero or more "%t" lines.
-# - Next, zero or more "%o" lines.
-# - Next, zero or more "%Y" lines.
-# - Next, a single "%K" line.
-# - Next, an optional "%O" line.
-# - Next, an optional "%A" line.
-# - Next, zero or more "%E" lines.
-
 expected_directive_order = re.compile("I(?:S|ST|STU)(?:|V|VW|VWX)NC*D*H*F*e*p*t*o*Y*KO?A?E*$")
 
 identification_patterns = [re.compile(pattern) for pattern in [
             "N[0-9]{4}$",
             "M[0-9]{4}$",
+            "M[0-9]{4} M[0-9]{4}$",
             "M[0-9]{4} N[0-9]{4}$",
             "M[0-9]{4} N[0-9]{4} N[0-9]{4}$"
         ]
     ]
 
-# The expected keywords are documented in two places:
+# The expected keywords are documented in three places:
 #
 # http://oeis.org/eishelp1.html
 # http://oeis.org/eishelp2.html                                (more elaborate than the first; documents the keywords "changed", "hear", and "look").
 # http://oeis.org/wiki/User:Charles_R_Greathouse_IV/Keywords   (more up-to-date).
 
 expected_keywords = [
-    "allocated", #
+    "allocated",  #
     "allocating", #
-    "base",      # dependent on base used for sequence
-    "bref",      # sequence is too short to do any analysis with
-    "changed",   #
-    "cofr",      # a continued fraction expansion of a number
-    "cons",      # a decimal expansion of a number
-    "core",      # an important sequence
-    "dead",      # an erroneous sequence
-    "dumb",      # an unimportant sequence
-    "dupe",      # duplicate of another sequence
-    "easy",      # it is very easy to produce terms of sequence
-    "eigen",     # an eigensequence: a fixed sequence for some transformation
-    "fini",      # a finite sequence
-    "frac",      # numerators or denominators of sequence of rationals
-    "full",      # the full sequence is given
-    "hard",      # next term not known, may be hard to find. Would someone please extend this sequence?
-    "hear",
-    "less",      # reluctantly accepted
-    "look",
-    "more",      # more terms are needed! would someone please extend this sequence?
-    "mult",      # multiplicative: a(mn)=a(m)a(n) if g.c.d.(m,n)=1
-    "new",       # new (added within last two weeks, roughly)
-    "nice",      # an exceptionally nice sequence
-    "nonn",      # a sequence of nonnegative numbers
-    "obsc",      # obscure, better description needed
-    "probation",
-    "sign",      # sequence contains negative numbers
-    "tabf",      # An irregular (or funny-shaped) array of numbers made into a sequence by reading it row by row
-    "tabl",      # typically a triangle of numbers, such as Pascal's triangle, made into a sequence by reading it row by row
-    "uned",      # not edited
-    "unkn",      # little is known; an unsolved problem; anyone who can find a formula or recurrence is urged to let me know.
-    "walk",      # counts walks (or self-avoiding paths)
-    "word"       # depends on words for the sequence in some language
+    "base",       # dependent on base used for sequence
+    "bref",       # sequence is too short to do any analysis with
+    "changed",    #
+    "cofr",       # a continued fraction expansion of a number
+    "cons",       # a decimal expansion of a number
+    "core",       # an important sequence
+    "dead",       # an erroneous sequence
+    "dumb",       # an unimportant sequence
+    "dupe",       # duplicate of another sequence
+    "easy",       # it is very easy to produce terms of sequence
+    "eigen",      # an eigensequence: a fixed sequence for some transformation
+    "fini",       # a finite sequence
+    "frac",       # numerators or denominators of sequence of rationals
+    "full",       # the full sequence is given
+    "hard",       # next term not known, may be hard to find. Would someone please extend this sequence?
+    "hear",       #
+    "less",       # reluctantly accepted
+    "look",       #
+    "more",       # more terms are needed! would someone please extend this sequence?
+    "mult",       # multiplicative: a(mn)=a(m)a(n) if g.c.d.(m,n)=1
+    "new",        # new (added within last two weeks, roughly)
+    "nice",       # an exceptionally nice sequence
+    "nonn",       # a sequence of nonnegative numbers
+    "obsc",       # obscure, better description needed
+    "probation",  #
+    "recycled",   #
+    "sign",       # sequence contains negative numbers
+    "tabf",       # An irregular (or funny-shaped) array of numbers made into a sequence by reading it row by row
+    "tabl",       # typically a triangle of numbers, such as Pascal's triangle, made into a sequence by reading it row by row
+    "uned",       # not edited
+    "unkn",       # little is known; an unsolved problem; anyone who can find a formula or recurrence is urged to let me know.
+    "walk",       # counts walks (or self-avoiding paths)
+    "word"        # depends on words for the sequence in some language
 ]
 
 expected_keywords_set = frozenset(expected_keywords)
@@ -123,11 +78,60 @@ bfile_line_pattern = re.compile("(-?[0-9]+)[ \t]+(-?[0-9]+)")
 
 def parse_main_content(oeis_id, main_content):
 
+    # As described here: https://oeis.org/eishelp1.html
+
+    # The directives %S, %T, and %U were originally intended as the absolute values of the sequence entries,
+    # with the signed entries given as %V, %W, %X. However, in the versions we downloaded, %S, %T, and %U
+    # are signed, and %V, %W, and %X are never present.
+
+    #expected_directives = [
+    #    "%I", # Identification Line                                     (REQUIRED)
+    #    "%S", # Beginning f the sequence (line 1 of 3)                  (REQUIRED)
+    #    "%T", # Beginning f the sequence (line 2 of 3)
+    #    "%U", # Beginning f the sequence (line 3 of 3)
+    #    "%N", # Name of sequence                                        (REQUIRED)
+    #    "%D", # Detailed references
+    #    "%H", # Links related to this sequence
+    #    "%F", # Formula
+    #    "%Y", # Cross-references to other sequences
+    #    "%A", # Author, submitter, or other Authority                   (REQUIRED)
+    #    "%O", # Offset a, b                                             (REQUIRED)
+    #    "%p", # Computer program to produce the sequence (Maple)
+    #    "%t", # Computer program to produce the sequence (Mathematica)
+    #    "%o", # Computer program to produce the sequence (other computer language)
+    #    "%E", # Extensions and errors
+    #    "%e", # Examples
+    #    "%K", # Keywords                                                (REQUIRED)
+    #    "%C"  # Comments
+    #]
+
+    # The order of expected directives, for any given entry is as follows:
+    #
+    # - First, a single "%I" entry.
+    # - Next, either a single "%S" line, an "%S" line followed by a "%T" line, or an "%S" line followed by a "%T" line followed by a "%U" line.
+    # - Next, either a single "%V" line, a "%V" line followed by a "%W" line, or a "%V" line followed by a "%W" line followed by an "%X" line.
+    # - Next, a single "%N" line.
+    # - Next, zero or more "%C" lines.
+    # - Next, zero or more "%D" lines.
+    # - Next, zero or more "%H" lines.
+    # - Next, zero or more "%F" lines.
+    # - Next, zero or more "%e" lines.
+    # - Next, zero or more "%p" lines.
+    # - Next, zero or more "%t" lines.
+    # - Next, zero or more "%o" lines.
+    # - Next, zero or more "%Y" lines.
+    # - Next, a single "%K" line.
+    # - Next, an optional "%O" line.
+    # - Next, an optional "%A" line.
+    # - Next, zero or more "%E" lines.
+
     # Select only lines that have the proper directive format.
 
     directive_line_pattern = "%(.) A{:06d}(.*)$".format(oeis_id)
 
     lines = re.findall(directive_line_pattern, main_content, re.MULTILINE)
+
+    # remove space between Axxxxxx and value
 
     new_lines = []
     for (directive, directive_value) in lines:
@@ -141,6 +145,19 @@ def parse_main_content(oeis_id, main_content):
 
     lines = new_lines
     del new_lines
+
+    if True: # check format
+
+        header = "# Greetings from The On-Line Encyclopedia of Integer Sequences! http://oeis.org/\n\nSearch: id:a{:06d}\nShowing 1-1 of 1\n\n".format(oeis_id)
+
+        check_main_content = ["%{} A{:06d}{}{}\n".format(directive, oeis_id, "" if directive_value == "" else " ", directive_value) for (directive, directive_value) in lines]
+
+        footer = "\n# Content is available under The OEIS End-User License Agreement: http://oeis.org/LICENSE\n"
+
+        check_main_content = header + "".join(check_main_content) + footer
+
+        if main_content != check_main_content:
+            logger.warning("[A{:06}] (P17) Main content reconstruction failed.".format(oeis_id))
 
     # ========== check order of directives
 
@@ -385,9 +402,9 @@ def parse_bfile_content(oeis_id, bfile_content):
 def parse_oeis_content(oeis_id, main_content, bfile_content):
 
     (identification, main_values, name, offset, keywords) = parse_main_content (oeis_id, main_content)
-    (bfile_first_index, bfile_values)                    = parse_bfile_content(oeis_id, bfile_content)
+    (bfile_first_index, bfile_values)                     = parse_bfile_content(oeis_id, bfile_content)
 
-    # Merge values obtained from S/T/U directives in main_content width b-file values.
+    # Merge values obtained from S/T/U or V/W/X directives in main_content with the b-file values.
 
     if not len(bfile_values) >= len(main_values):
         logger.warning("[A{:06}] (P7) Main file has more values than b-file (main: {}, b-file: {}).".format(oeis_id, len(main_values), len(bfile_values)))
@@ -415,7 +432,7 @@ def parse_oeis_content(oeis_id, main_content, bfile_content):
     if len(offset) > 1 and (offset[1] != first_index_where_magnitude_exceeds_1):
         logger.error("[A{:06}] (P9) %O directive claims first index where magnitude exceeds 1 is {}, but values suggest this should be {}.".format(oeis_id, offset[1], first_index_where_magnitude_exceeds_1))
 
-    # ========== return parsed values
+    # Return parsed values.
 
     return OeisEntry(oeis_id, identification, values, name, offset, keywords)
 
