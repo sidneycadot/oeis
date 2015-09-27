ca663e40ccc9ff12da897a0eff9bf344c8e7356e
Sidney Cadot <sidney@jigsaw.nl>
2015-09-27 19:59:00 +0200

    Added scope exitors.

---
+++ exit_scope.py
@@ -0,0 +1,18 @@
+
+class ExitScopeContextManager:
+
+    def __init__(self, instance, operation):
+        self.instance  = instance
+        self.operation = operation
+
+    def __enter__(self):
+        return self.instance
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.operation(self.instance)
+
+def close_when_done(instance):
+    return ExitScopeContextManager(instance, lambda x : x.close())
+
+def shutdown_when_done(instance):
+    return ExitScopeContextManager(instance, lambda x : x.shutdown())
