4544a9e350a9044e10d368580e41dd40061e07ef
Jesse Weinstein <jesse@wefu.org>
2019-04-18 19:48:19 -0700

    Use namedtuple

--- oeis_entry.py
+++ oeis_entry.py
@@ -10,29 +10,10 @@ from exit_scope    import close_when_done
 
 logger = logging.getLogger(__name__)
 
-class OeisEntry:
-    def __init__(self, oeis_id, identification, values, name, comments, detailed_references, links, formulas, examples,
-                 maple_programs, mathematica_programs, other_programs, cross_references, keywords, offset_a, offset_b, author, extensions_and_errors):
-        self.oeis_id               = oeis_id
-        self.identification        = identification
-        self.values                = values
-        self.name                  = name
-        self.comments              = comments
-        self.detailed_references   = detailed_references
-        self.links                 = links
-        self.formulas              = formulas
-        self.examples              = examples
-        self.maple_programs        = maple_programs
-        self.mathematica_programs  = mathematica_programs
-        self.other_programs        = other_programs
-        self.cross_references      = cross_references
-        self.keywords              = keywords
-        self.offset_a              = offset_a
-        self.offset_b              = offset_b
-        self.author                = author
-        self.extensions_and_errors = extensions_and_errors
-    def __str__(self):
-        return "A{:06d}".format(self.oeis_id)
+OeisEntry = collections.namedtuple('OeisEntry', """oeis_id, identification,
+   values, name, comments, detailed_references, links, formulas, examples,
+   maple_programs, mathematica_programs, other_programs, cross_references,
+   keywords, offset_a, offset_b, author, extensions_and_errors""")
 
 expected_directive_order = re.compile("I(?:S|ST|STU)(?:|V|VW|VWX)NC*D*H*F*e*p*t*o*Y*KO?A?E*$")
 
