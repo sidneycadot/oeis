753869002694504bb15f4bf09f3ce6cea770a15c
Sidney Cadot <sidney@jigsaw.nl>
2023-01-01 18:03:59 +0100

    Improved comments.

--- source/utilities/exit_scope.py
+++ source/utilities/exit_scope.py
@@ -1,6 +1,8 @@
+"""Provide the ExitScopeContextManager class and end-of-context utility functions."""
+
 
 class ExitScopeContextManager:
-
+    """A context manager that invokes an operation on a given instance when the control flow leaves the context."""
     def __init__(self, instance, operation):
         self.instance  = instance
         self.operation = operation
@@ -8,13 +10,15 @@ class ExitScopeContextManager:
     def __enter__(self):
         return self.instance
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exception_type, exception_value, exception_traceback):
         self.operation(self.instance)
 
 
 def close_when_done(instance):
+    """Returns a context that will call `close` method of the passed instance when leaving the context."""
     return ExitScopeContextManager(instance, lambda x : x.close())
 
 
 def shutdown_when_done(instance):
+    """Returns a context that will call `shutdown` method of the passed instance when leaving the context."""
     return ExitScopeContextManager(instance, lambda x : x.shutdown())
